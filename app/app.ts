a7df9a64aa7b1f85dd766c96216f5c224437850f
Jonathan Berteaux <berteauxj@gmail.com>
2015-06-29 22:18:08 +0200

    merge

--- app/app.ts
+++ app/app.ts
@@ -1,11 +1,9 @@
 /// <reference path="../typings/tsd.d.ts" />
-import {Component, View, bootstrap, NgFor, Parent, formDirectives, ControlGroup, Control, Validators} from 'angular2/angular2';
+import {Component, View, bootstrap, NgFor, Parent, forwardRef, formDirectives, ControlGroup, Control, Validators} from 'angular2/angular2';
 
 import {Employe} from 'class/employe';
 import {Poste} from 'class/poste';
 import {Departement} from 'class/departement';
-
-import {DepartementItem, PosteItem, EmployeItem} from 'components/organisationItem';
 
 import {StorageService} from 'services/storageService';
 import {DatasService} from 'services/datasService';
@@ -17,11 +15,18 @@ import {CustomValidator} from 'validators/customValidator';
 })
 @View({
     templateUrl: 'templates/index.html',
-    directives: [NgFor, DepartementItem, PosteItem, formDirectives]
+    directives: [
+        NgFor,
+        forwardRef(()=> DepartementItem),
+        forwardRef(()=> PosteItem),
+        forwardRef(()=> DepartementDetail),
+        formDirectives
+    ]
 })
-class Society {
+export class Society {
 	departements: Array<Departement>;
 	datasService: DatasService;
+    departementDetail: DepartementDetail;
     departementForm:ControlGroup;
 
 	constructor(customValidator: CustomValidator, datasService: DatasService) {
@@ -50,6 +55,105 @@ class Society {
     updateDepartement(position: number) {
 
     }
+
+    updateDepartementDetail(postes) {
+        this.departementDetail.updatePostes(postes);
+    }
+
+    registerDepartementDetail(departementDetail: DepartementDetail){
+        this.departementDetail = departementDetail;
+    }
 }
+
+@Component({
+    selector: 'departement',
+    appInjector: [DatasService]
+})
+@View({
+  templateUrl: 'templates/departement.html',
+    directives: [NgFor, forwardRef(()=> PosteItem)]
+})
+class DepartementDetail {
+    society:Society;
+    postes: Array<Poste>;
+
+    constructor(@Parent() society: Society){
+        this.society = society;
+        this.society.registerDepartementDetail(this);
+    }
+
+    updatePostes(postes) {
+        this.postes = postes;
+        console.log(this.postes);
+    }
+} 
+
+@Component({
+    selector: 'departement-item',
+    properties: ['departement']
+})
+@View({
+    templateUrl: 'templates/departement-item.html',
+    directives: [forwardRef(()=>PosteItem)]
+})
+export class DepartementItem
+{
+    departement: Departement;
+    society: Society;
+
+    constructor(@Parent() society: Society) {
+        this.society = society;
+    }
+
+    afficherPoste(postes) {
+        this.society.updateDepartementDetail(postes);
+    }
+}
+
+
+@Component({
+    selector: 'poste-item',
+    properties: ['poste']
+})
+@View({
+    template: `<h1>POSTE</h1>`,
+    directives: [forwardRef(()=>EmployeItem)]
+})
+export class PosteItem
+{
+    poste: Poste;
+    private departement: DepartementItem;
+
+    constructor(@Parent() departement: DepartementItem) {
+        this.departement = departement;
+    }
+
+    getDepartement() {
+        return this.departement;
+    }
+}
+
+
+@Component({
+    selector: 'employe-item',
+    properties: ['employe']
+})
+@View({
+    templateUrl: ''
+})
+export class EmployeItem
+{
+    employe: Employe;
+    private poste: PosteItem;
+
+    constructor(@Parent() poste: PosteItem) {
+        this.poste = poste;
+    }
+
+    getPoste() {
+        return this.poste;
+    }
+}
+
 
 bootstrap(Society);
